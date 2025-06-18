f92f32a9a781b6d4e40952688aa2ef79f045bc05
Rishabh Raj Shukla <rishabh.shukla@intelliconnectq.com>
2025-06-18 18:09:07 +0530

    feat: initialize login-v project with basic authentication functionality
    - Add Cargo.toml with dependencies for axum, sqlx, serde, and dotenv.
    - Create controller module with login functionality.
    - Implement get_authenticate function to handle user login requests.
    - Set up main application with Axum router and PostgreSQL connection.
    - Define model for login request with username and password fields.

---
+++ src/controller/login.rs
@@ -0,0 +1,49 @@
+use axum::{http::status, response::Response, Extension, Json};
+use hyper::Body;
+use serde_json::json;
+use sqlx::PgPool;
+
+use crate::model::login::{LoginReq, Order};
+
+
+pub async fn get_authenticate(
+    Extension(pg_pool): Extension<PgPool>,
+    Json(LoginReq { username, password }): Json<LoginReq>,
+) -> Result<Response<Body>, Response<Body>> {
+        
+    let res = sqlx::query(
+        r#"SELECT 
+        username, password
+        from users where username=$1 and password=$2"#,
+    )
+    .bind(&username)
+    .bind(password)
+    .fetch_one(&pg_pool)
+    .await;
+
+    match res {
+        Ok(_user) => {
+            let json = json!({
+                "username": username,
+                "staus": "Authenticated"
+            });
+            let body = Body::from(serde_json::to_vec(&json).unwrap());
+            Ok(Response::builder()
+                .status(status::StatusCode::OK)
+                .header("Content-Type", "application/json")
+                .body(body)
+                .unwrap())
+        }
+        Err(_e) => {
+            let json = json!({
+                "status": "Failed"
+            });
+            let body = Body::from(serde_json::to_vec(&json).unwrap());
+            Ok(Response::builder()
+                .status(status::StatusCode::UNAUTHORIZED)
+                .header("Content-Type", "application/json")
+                .body(body)
+                .unwrap())
+        }
+    }
+}
